e28ff856ce4bc52dfe35ce441d669a6170980b54
Bitrober <69868018+Robersoncassol@users.noreply.github.com>
2020-11-19 15:18:57 -0300

    Add files via upload

---
+++ Consultor.py
@@ -0,0 +1,12 @@
+print ("Cadastro de Consultor")
+
+def cadastro_consultor():
+
+    dados = dict()
+    dados ['nome'] = input('Nome')
+    dados ['área_de_atuação'] = input('Área de Atuação')
+    dados ['email'] = input('E-mail')
+    dados ['telefone'] = input('CNPJ/CPF')
+    dados ['lista_de_especialidades'] = input('Lista de Especialidades')
+    print(dados) 
+cadastro_consultor()
